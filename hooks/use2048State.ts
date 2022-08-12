14542b3b7490d9ad2b34a5e54ad426f2bc088840
Thao Bui <thuthao896@gmail.com>
2022-08-12 15:18:11 +0100

    2048/move left and up functions

--- hooks/use2048State.ts
+++ hooks/use2048State.ts
@@ -136,6 +136,102 @@ function reduce(
       }
     }
 
+    case ACTION_TYPE_MOVE_UP: {
+      const { activeTiles } = state
+      if (!activeTiles.length) return state
+
+      let tilesMoved = false
+      let newActiveTiles: ActiveTilesState = []
+
+      //   put active tiles in a board
+      let board = makeTilesBoard(TOTAL_ROWS, TOTAL_COLS, activeTiles)
+
+      // move up => for each column, merge from top
+      for (let col = 0; col < TOTAL_COLS; col++) {
+        let tilesInCol: TileState[] = []
+
+        //   get active tiles in column
+        for (let row = TOTAL_ROWS - 1; row > -1; row--) {
+          if (board[row][col]) {
+            tilesInCol.push(board[row][col])
+          }
+        }
+
+        //   fill column from top
+        let i = tilesInCol.length - 1
+        let currentRow = 0
+
+        while (i > -1 && currentRow < TOTAL_ROWS) {
+          let position = { r: currentRow, c: col }
+
+          if (i > 0 && tilesInCol[i].value == tilesInCol[i - 1]?.value) {
+            //  change position of 2 tiles to be merged
+            tilesMoved = true
+
+            //   animation delay: wait till old tiles moved to new position
+            let mergedTileAnimationDelay =
+              Math.abs(tilesInCol[i - 1].position.r - currentRow) *
+              TILE_ANIMATION_DELAY
+
+            tilesInCol[i - 1] = {
+              ...tilesInCol[i - 1],
+              prevPosition: { ...tilesInCol[i - 1].position },
+              position,
+              toBeRemoved: true,
+            }
+
+            tilesInCol[i] = {
+              ...tilesInCol[i],
+              prevPosition: { ...tilesInCol[i].position },
+              position,
+              toBeRemoved: true,
+            }
+
+            // add a merged Tile
+            tilesInCol.push({
+              value: tilesInCol[i].value * 2,
+              isMerged: true,
+              position,
+              animationDelay: mergedTileAnimationDelay,
+            })
+            i--
+          } else {
+            if (!tilesMoved) {
+              tilesMoved = tilesInCol[i].position.r != currentRow
+            }
+
+            tilesInCol[i] = {
+              ...tilesInCol[i],
+              prevPosition: { ...tilesInCol[i].position },
+              position,
+            }
+          }
+
+          currentRow++
+          i--
+        }
+
+        newActiveTiles = [...newActiveTiles, ...tilesInCol]
+      }
+
+      //   if there's movement then add a new tile
+      if (tilesMoved) {
+        const emptyTiles = getEmptyTiles(newActiveTiles, ALL_TILES_POS)
+        if (emptyTiles.length) {
+          newActiveTiles.push({
+            isNew: true,
+            value: 2,
+            position: getRandomEmptyTile(emptyTiles),
+          })
+        }
+      }
+
+      return {
+        ...state,
+        activeTiles: newActiveTiles,
+      }
+    }
+
     case ACTION_TYPE_MOVE_RIGHT: {
       const { activeTiles } = state
       if (!activeTiles.length) return state
@@ -208,6 +304,101 @@ function reduce(
           }
 
           currentCol--
+          i--
+        }
+
+        newActiveTiles = [...newActiveTiles, ...tilesInRow]
+      }
+
+      //   if there's movement then add a new tile
+      if (tilesMoved) {
+        const emptyTiles = getEmptyTiles(newActiveTiles, ALL_TILES_POS)
+        if (emptyTiles.length) {
+          newActiveTiles.push({
+            isNew: true,
+            value: 2,
+            position: getRandomEmptyTile(emptyTiles),
+          })
+        }
+      }
+
+      return {
+        ...state,
+        activeTiles: newActiveTiles,
+      }
+    }
+    case ACTION_TYPE_MOVE_LEFT: {
+      const { activeTiles } = state
+      if (!activeTiles.length) return state
+
+      let tilesMoved = false
+      let newActiveTiles: ActiveTilesState = []
+
+      //   put active tiles in a board
+      let board = makeTilesBoard(TOTAL_ROWS, TOTAL_COLS, activeTiles)
+
+      // move left => for each row, merge from left
+      for (let row = 0; row < TOTAL_ROWS; row++) {
+        let tilesInRow: TileState[] = []
+
+        //   get active tiles in row
+        for (let col = TOTAL_COLS - 1; col > -1; col--) {
+          if (board[row][col]) {
+            tilesInRow.push(board[row][col])
+          }
+        }
+
+        //   fill row from left
+        let i = tilesInRow.length - 1
+        let currentCol = 0
+
+        while (i > -1 && currentCol < TOTAL_COLS) {
+          let position = { r: row, c: currentCol }
+
+          if (i > 0 && tilesInRow[i].value == tilesInRow[i - 1]?.value) {
+            //  change position of 2 tiles to be merged
+            tilesMoved = true
+
+            //   animation delay: wait till old tiles moved to new position
+            let mergedTileAnimationDelay =
+              Math.abs(tilesInRow[i - 1].position.c - currentCol) *
+              TILE_ANIMATION_DELAY
+
+            tilesInRow[i - 1] = {
+              ...tilesInRow[i - 1],
+              prevPosition: { ...tilesInRow[i - 1].position },
+              position,
+              toBeRemoved: true,
+            }
+
+            tilesInRow[i] = {
+              ...tilesInRow[i],
+              prevPosition: { ...tilesInRow[i].position },
+              position,
+              toBeRemoved: true,
+            }
+
+            // add a merged Tile
+            tilesInRow.push({
+              value: tilesInRow[i].value * 2,
+              isMerged: true,
+              position,
+              animationDelay: mergedTileAnimationDelay,
+            })
+            i--
+          } else {
+            if (!tilesMoved) {
+              tilesMoved = tilesInRow[i].position.c != currentCol
+            }
+
+            tilesInRow[i] = {
+              ...tilesInRow[i],
+              prevPosition: { ...tilesInRow[i].position },
+              position,
+            }
+          }
+
+          currentCol++
           i--
         }
 
