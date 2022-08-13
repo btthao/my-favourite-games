1fe973925c4d9efa068de0181c1507ecbcf4bff7
Thao Bui <thuthao896@gmail.com>
2022-08-13 23:54:41 +0100

    config sass options to prepend variables and mixins

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
+  sassOptions: {
+    includePaths: ['./styles/theme'],
+    prependData: `@use "variables" as v; @use "mixins" as m;`,
+  },
 }
 
 module.exports = nextConfig
