50bfa6705bbe973827447265cc93c76f886b7f6d
Thao Bui <thuthao896@gmail.com>
2022-08-17 23:23:33 +0100

    add menu folders

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import 'styles/globals.scss'
 import type { AppProps } from 'next/app'
+import Menu from 'components/Menu'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="page-wrapper">
+      <Menu />
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
