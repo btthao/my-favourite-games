d0a76b5d340f1c8b294e091018d8eb6f7a43cef6
Thao Bui <thuthao896@gmail.com>
2023-02-26 15:05:31 +0000

    menubar layout

---
+++ components/window/MenuBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from 'styles/MenuBar.module.scss'
+
+interface MenuBarProps {
+  children: JSX.Element[]
+}
+
+const MenuBar: React.FC<MenuBarProps> = ({ children }) => {
+  return <div className={styles.container}>{children}</div>
+}
+
+export default MenuBar
